62663351d289843ee45feaa11ebee9bd739096b6
Sergio Gonzalez <sergio.gonzalez21499@gmail.com>
2021-07-04 00:39:34 -0500

    started hw2 off with existing cv_ellipse code

--- cv_hw/hw2/program2.cpp
+++ cv_hw/hw2/program2.cpp
@@ -1,12 +1,12 @@
 /*
 Sergio Gonzalez
-CSE 4310 HW1
+CSE 4310 HW2
 
-This program was built on top of the existing 'cv_clickable'.
+This program was built on top of the existing 'cv_ellipse'.
 */
 
 //
-//    Copyright 2021 Christopher D. McMurrough
+//    Copyright 2018 Christopher D. McMurrough
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -23,124 +23,18 @@ This program was built on top of the existing 'cv_clickable'.
 //
 
 /*******************************************************************************************************************//**
- * @file cv_clickable.cpp
- * @brief C++ example of basic interaction with an image in OpenCV
+ * @file cv_ellipse.cpp
+ * @brief C++ example of Canny edge detection and ellipse model fitting in OpenCV
  * @author Christopher D. McMurrough
  **********************************************************************************************************************/
 
 // include necessary dependencies
 #include <iostream>
-#include <stdbool.h>
 #include <string>
 #include "opencv2/opencv.hpp"
 
-
-// global variables
-std::string inputFileName; // contains filename
-int current_tool = 0; // used to keep track of current tool 
-bool printed = false; // flag used to track whether the current tool has been printed to the console
-bool drawing = false; // flag for pencil tool engagement, set to true when left click, false when left click released
-cv::Mat imageIn; // container for image
-cv::Vec3b eyedrop_sample(255,255,255); // color in eyedrop tool, always starts as white
-cv::Point p1, p2; // point variables for crop tool
-
-/*******************************************************************************************************************//**
- * @brief handler for image click callbacks
- * @param[in] event number of command line arguments
- * @param[in] x string array of command line arguments
- * @param[in] y string array of command line arguments
- * @param[in] flags string array of command line arguments
- * @param[in] userdata string array of command line arguments
- * @return return code (0 for normal termination)
- * @author Christoper D. McMurrough
- **********************************************************************************************************************/
-static void clickCallback(int event, int x, int y, int flags, void* userdata)
-{	
-	
-    if (printed == false)
-    {
-    	// enumeration of the tools is as below
-        if (current_tool == 0)
-            std::cout << "CURRENT TOOL: EYEDROPPER" << std::endl;
-        else if (current_tool == 1)
-            std::cout << "CURRENT TOOL: CROP" << std::endl;
-        else if (current_tool == 2)
-            std::cout << "CURRENT TOOL: PENCIL" << std::endl;
-        else if (current_tool == 3)
-            std::cout << "CURRENT TOOL: PAINTBUCKET" << std::endl;
-        else if (current_tool == 4)
-            std::cout << "CURRENT TOOL: RESET" << std::endl;
-        
-	printed = true;
-    }
-
-    if(event == cv::EVENT_LBUTTONDOWN)
-    {
-        if(current_tool == 0)
-        {
-        	if((eyedrop_sample[0] != imageIn.at<cv::Vec3b>(y,x)[0]) || (eyedrop_sample[1] != imageIn.at<cv::Vec3b>(y,x)[1]) || (eyedrop_sample[2] != imageIn.at<cv::Vec3b>(y,x)[2]))
-        	{
-			eyedrop_sample = imageIn.at<cv::Vec3b>(y,x);
-			std::cout << "\tCURRENT COLOR: " << (int)eyedrop_sample[0] << "-" << (int)eyedrop_sample[1] << "-" << (int)eyedrop_sample[2] << std::endl;
-        	}
-        }
-        else if(current_tool == 1)
-        {
-        	p1 = cv::Point(x,y);
-        }
-        else if(current_tool == 2)
-        {
-        	drawing = true;
-        	imageIn.at<cv::Vec3b>(y,x) = eyedrop_sample;
-        	
-        	cv::imshow("imageIn", imageIn);
-    		cv::waitKey();
-        }
-        else if(current_tool == 3)
-        {
-        	floodFill(imageIn, cv::Point(x,y), eyedrop_sample, 0, cv::Scalar(), cv::Scalar(), 4);
-        	
-        	cv::imshow("imageIn", imageIn);
-    		cv::waitKey();
-        }
-        
-    }
-    else if(event == cv::EVENT_RBUTTONDOWN)
-    {
-    	// this code block iterates throughout the tools, and flips the 'printed' variable to false
-        current_tool++;
-        current_tool %= 5;
-        
-        printed = false;
-    }
-    else if((event == cv::EVENT_MOUSEMOVE) && (drawing == true) && (current_tool == 2))
-    {
-	imageIn.at<cv::Vec3b>(y,x) = eyedrop_sample;
-	cv::imshow("imageIn", imageIn);
-	cv::waitKey();
-    }
-    else if(event == cv::EVENT_LBUTTONUP)
-    {
-    	if(current_tool == 1)
-    	{
-    		p2 = cv::Point(x,y);
-    		
-    		imageIn = imageIn(cv::Rect(p1,p2));
-    		cv::imshow("imageIn", imageIn);
-	    	cv::waitKey();
-    	}
-    	else if(current_tool == 2)
-    	{
-    		drawing = false;
-    	}
-    }
-    else if(event == cv::EVENT_LBUTTONDBLCLK && (current_tool == 4))
-    {
-        imageIn = cv::imread(inputFileName, cv::IMREAD_COLOR);
-	cv::imshow("imageIn", imageIn);
-    	cv::waitKey();
-    }
-}
+// configuration parameters
+#define NUM_COMNMAND_LINE_ARGUMENTS 1
 
 /*******************************************************************************************************************//**
  * @brief program entry point
@@ -151,19 +45,107 @@ static void clickCallback(int event, int x, int y, int flags, void* userdata)
  **********************************************************************************************************************/
 int main(int argc, char **argv)
 {
-    // open the input image
-    inputFileName = argv[1];
-    imageIn = cv::imread(inputFileName, cv::IMREAD_COLOR);
+    cv::Mat imageIn;
 
-    // check for file error
-    if(!imageIn.data)
+    // validate and parse the command line arguments
+    if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
     {
-        std::cout << "Error while opening file " << inputFileName << std::endl;
+        std::printf("USAGE: %s <image_path> \n", argv[0]);
         return 0;
     }
+    else
+    {
+        imageIn = cv::imread(argv[1], cv::IMREAD_COLOR);
 
+        // check for file error
+        if(!imageIn.data)
+        {
+            std::cout << "Error while opening file " << argv[1] << std::endl;
+            return 0;
+        }
+    }
+
+    // get the image size
+    std::cout << "image width: " << imageIn.size().width << std::endl;
+    std::cout << "image height: " << imageIn.size().height << std::endl;
+    std::cout << "image channels: " << imageIn.channels() << std::endl;
+
+    // convert the image to grayscale
+    cv::Mat imageGray;
+    cv::cvtColor(imageIn, imageGray, cv::COLOR_BGR2GRAY);
+
+    // find the image edges
+    cv::Mat imageEdges;
+    const double cannyThreshold1 = 100;
+    const double cannyThreshold2 = 200;
+    const int cannyAperture = 3;
+    cv::Canny(imageGray, imageEdges, cannyThreshold1, cannyThreshold2, cannyAperture);
+
+    // locate the image contours (after applying a threshold or canny)
+    std::vector<std::vector<cv::Point> > contours;
+    cv::findContours(imageEdges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+    // draw the contours
+    cv::Mat imageContours = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    cv::RNG rand(12345);
+    for(int i = 0; i < contours.size(); i++)
+    {
+        cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+        cv::drawContours(imageContours, contours, i, color);
+    }
+
+    // compute minimum area bounding rectangles
+    std::vector<cv::RotatedRect> minAreaRectangles(contours.size());
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // compute a minimum area bounding rectangle for the contour
+        minAreaRectangles[i] = cv::minAreaRect(contours[i]);
+    }
+
+    // draw the rectangles
+    cv::Mat imageRectangles = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    for(int i = 0; i < contours.size(); i++)
+    {
+        cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+        cv::Point2f rectanglePoints[4];
+        minAreaRectangles[i].points(rectanglePoints);
+        for(int j = 0; j < 4; j++)
+        {
+            cv::line(imageRectangles, rectanglePoints[j], rectanglePoints[(j+1) % 4], color);
+        }
+    }
+
+    // fit ellipses to contours containing sufficient inliers
+    std::vector<cv::RotatedRect> fittedEllipses(contours.size());
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // compute an ellipse only if the contour has more than 5 points (the minimum for ellipse fitting)
+        if(contours.at(i).size() > 5)
+        {
+            fittedEllipses[i] = cv::fitEllipse(contours[i]);
+        }
+    }
+
+    // draw the ellipses
+    cv::Mat imageEllipse = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    const int minEllipseInliers = 500;
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // draw any ellipse with sufficient inliers
+        if(contours.at(i).size() > minEllipseInliers)
+        {
+            cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+            cv::ellipse(imageEllipse, fittedEllipses[i], color, 2);
+        }
+    }
+
+    // display the images
     cv::imshow("imageIn", imageIn);
-    cv::setMouseCallback("imageIn", clickCallback, &imageIn);
+    cv::imshow("imageGray", imageGray);
+    cv::imshow("imageEdges", imageEdges);
+    cv::imshow("imageContours", imageContours);
+    cv::imshow("imageRectangles", imageRectangles);
+    cv::imshow("imageEllipse", imageEllipse);
     cv::waitKey();
 }
 
