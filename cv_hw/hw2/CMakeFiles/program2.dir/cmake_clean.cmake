38f4ff9b80fb4cf2635b7128b7112621531841e4
Sergio Gonzalez <sergio.gonzalez21499@gmail.com>
2021-07-04 03:26:06 -0500

    comments

---
+++ cv_hw/hw2/CMakeFiles/program2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program2.dir/program2.cpp.o"
+  "program2"
+  "program2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
