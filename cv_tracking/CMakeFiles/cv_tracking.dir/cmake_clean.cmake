ad70efd7b399ca0a9c13dc8a8484207058879047
Sergio Gonzalez <sergio.gonzalez21499@gmail.com>
2021-07-26 17:27:50 -0500

    minor changes, prep for hw3

---
+++ cv_tracking/CMakeFiles/cv_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_tracking.dir/cv_tracking.cpp.o"
+  "cv_tracking"
+  "cv_tracking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
