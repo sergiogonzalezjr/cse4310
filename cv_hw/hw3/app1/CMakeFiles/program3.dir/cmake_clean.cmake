541b7b42b705b6880185485cf2c3ee4ada2d7699
Sergio Gonzalez <sergio.gonzalez21499@gmail.com>
2021-07-28 23:56:04 -0500

    counting fully functional

---
+++ cv_hw/hw3/app1/CMakeFiles/program3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program3.dir/program3.cpp.o"
+  "program3"
+  "program3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cv_hw/hw3/app2/CMakeFiles/cv_pmog.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sergiog/Documents/cse4310/cv_hw/hw3/app2/cv_pmog.cpp" "/home/sergiog/Documents/cse4310/cv_hw/hw3/app2/CMakeFiles/cv_pmog.dir/cv_pmog.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
