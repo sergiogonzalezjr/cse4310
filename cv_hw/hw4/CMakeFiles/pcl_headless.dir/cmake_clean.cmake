ccafaf4f5196ec80e226ffad7062a33731f1eb4e
Sergio Gonzalez <sergio.gonzalez21499@gmail.com>
2021-08-10 03:28:57 -0500

    pcl_headless test run

---
+++ cv_hw/hw4/CMakeFiles/pcl_headless.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_headless.dir/pcl_headless.cpp.o"
+  "pcl_headless"
+  "pcl_headless.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_headless.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
